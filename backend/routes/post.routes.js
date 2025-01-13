29ca42b449e98f43fa51b2316117d477ebcdff4c
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-01-13 22:38:08 +0700

    2- post routes and controller added

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+import { protectRoute } from "../middlewares/protectRoute.js";
+import {
+  commentOnPost,
+  createPost,
+  deletePost,
+  getAllPosts,
+  getFollowingPost,
+  getLikedPosts,
+  getUserPosts,
+  likeUnlikePost,
+} from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPost);
+router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
+router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.delete("/:id", protectRoute, deletePost);
+
+export default router;
