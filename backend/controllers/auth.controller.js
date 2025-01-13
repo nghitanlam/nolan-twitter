6f592158c3f9188c652e4ce6c96cfb8ffbed0974
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-01-13 13:20:27 +0700

    1- Project Setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = (req, res) => {
+  res.json({
+    data: "Signup",
+  });
+};
+
+export const login = (req, res) => {
+  res.json({
+    data: "Login",
+  });
+};
+
+export const logout = (req, res) => {
+  res.json({
+    data: "Logout",
+  });
+};
