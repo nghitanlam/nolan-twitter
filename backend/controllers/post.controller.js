5a8fff418f0588e8ada62529a6cd19cab3fe39ab
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-01-14 16:11:55 +0700

    Fix GetUserProfile route

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,8 +1,8 @@
+import { v2 as cloudinary } from "cloudinary";
+
 import Notification from "../models/notification.model.js";
 import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
-
-import { v2 as cloudinary } from "cloudinary";
 
 export const createPost = async (req, res) => {
   try {
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -11,6 +11,8 @@ export const getUserProfile = async (req, res) => {
     const user = await User.findOne({ username }).select("-password");
 
     if (!user) return res.status(404).json({ error: "User not found" });
+
+    res.status(200).json(user);
   } catch (error) {
     console.log("Error in getUserProfile: ", error.message);
     res.status(500).json({ error: error.message });
